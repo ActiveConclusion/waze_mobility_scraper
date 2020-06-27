f73a965776320748acfd926b1416827a60189aa1
ActiveConclusion <activeconclusioner@gmail.com>
2020-06-27 23:30:48 +0300

    Update source.py

--- source.py
+++ source.py
@@ -31,10 +31,14 @@ def scrape_data(email, password):
         driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
         driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(Keys.ENTER)
         print('Pass')
+
         # driver.save_screenshot("pass.png")
 
         time.sleep(5*M)
         driver.save_screenshot("log aft.png")
+        driver.find_element_by_xpath("//*[contains(text(), 'Try another way')]").click()
+        time.sleep(5*M)
+        driver.save_screenshot("another way.png")
         # download country-level dara
         driver.get('https://datastudio.google.com/embed/reporting/fe8a3c7d-9303-4e70-8acb-4e042714fa76/page/bhuOB')
         time.sleep(3*M)
