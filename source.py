21ebd4122b5022422c1d187944fce6e7a680d82a
ActiveConclusion <activeconclusioner@gmail.com>
2020-06-27 22:24:59 +0300

    Update source.py

--- source.py
+++ source.py
@@ -10,9 +10,11 @@ import os
 from pathlib import Path
 from pyvirtualdisplay import Display
 
+M = 2
 def scrape_data(email, password):
-    display = Display(visible=0, size=(1000, 1000))  
+    display = Display(visible=0, size=(1200, 1200))  
     display.start()
+     
     driver = webdriver.Chrome(ChromeDriverManager().install())
 
     driver.get('https://stackoverflow.com/users/signup')
@@ -22,30 +24,30 @@ def scrape_data(email, password):
     driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(email)
     driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(Keys.ENTER)
 
-    driver.implicitly_wait(8)
+    driver.implicitly_wait(8 * M)
 
     driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
     driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(Keys.ENTER)
 
-    time.sleep(5)
+    time.sleep(5*M)
     # download country-level dara
     driver.get('https://datastudio.google.com/embed/reporting/fe8a3c7d-9303-4e70-8acb-4e042714fa76/page/bhuOB')
-    time.sleep(3)
+    time.sleep(3*M)
     driver.find_element_by_xpath("//button[@class='lego-control md-button md-data-studio-theme md-ink-ripple']").click()
     driver.find_element_by_xpath("//div[@class='md-label']").click()
-    time.sleep(5)
+    time.sleep(5*M)
     driver.find_elements_by_xpath("//div[@class='chart-menu-button header-menu-button']")[1].click()
     driver.find_element_by_xpath("//*[contains(text(), 'Download CSV')]").click()
-    time.sleep(5)
+    time.sleep(5*M)
     # download city-level data
     driver.get('https://datastudio.google.com/embed/reporting/fe8a3c7d-9303-4e70-8acb-4e042714fa76/page/epuOB')
-    time.sleep(3)
+    time.sleep(3*M)
     driver.find_element_by_xpath("//button[@class='lego-control md-button md-data-studio-theme md-ink-ripple']").click()
     driver.find_element_by_xpath("//div[@class='md-label']").click()
-    time.sleep(5)
+    time.sleep(5*M)
     driver.find_elements_by_xpath("//div[@class='chart-menu-button header-menu-button']")[1].click()
     driver.find_element_by_xpath("//*[contains(text(), 'Download CSV')]").click()
-    time.sleep(5)
+    time.sleep(5*M)
     driver.quit()
     display.stop()
     print(os.listdir(os.path.join(Path.home(), "Downloads")))
